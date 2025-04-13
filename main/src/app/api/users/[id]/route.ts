4acf41a47624b4f124088958daf536db3bccc5fd
SynapsLimited <synapslimited@gmail.com>
2025-04-13 08:39:21 +0200

    Update route.ts

--- main/src/app/api/users/[id]/route.ts
+++ main/src/app/api/users/[id]/route.ts
@@ -2,10 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '../../lib/db';
 import User from '../../lib/models/userModel';
 
+// GET handler for fetching a user by ID
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   await connectDB();
   try {
-    const user = await User.findById(params.id).select('name');
+    const { id } = params; // Access id from params
+    const user = await User.findById(id).select('name');
     if (!user) {
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
