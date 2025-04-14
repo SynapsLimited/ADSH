171fa3e197fb82d72dcdd57d8cf44627c2ed0809
SynapsLimited <synapslimited@gmail.com>
2025-04-14 13:53:23 +0200

    .

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -51,4 +52,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+} satisfies Config;
